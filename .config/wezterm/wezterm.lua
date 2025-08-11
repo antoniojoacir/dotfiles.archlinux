64da8c5ca97c3eb539c9a45872935615be593c01
antoniojoacir <antonjoacir@gmail.com>
2025-08-11 11:57:06 -0300

    wezterm: bem mais rapido que o ghostty

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+return {
+	enable_wayland = false,
+	font = wezterm.font("Iosevka Nerd Font Mono"),
+	font_size = 14,
+	enable_tab_bar = false,
+	window_background_opacity = 0.95,
+	color_scheme = "Black Metal (Bathory) (base16)",
+	adjust_window_size_when_changing_font_size = false,
+	default_cursor_style = "BlinkingBar",
+	cursor_blink_rate = 500,
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_ease_out = "Constant",
+}
