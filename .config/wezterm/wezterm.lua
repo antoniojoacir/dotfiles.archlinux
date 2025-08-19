d9bd4ddc5eae12daba9b9d9a4625235b91262607
antoniojoacir <antonjoacir@gmail.com>
2025-08-19 10:09:57 -0300

    atualizando as configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,12 +2,17 @@ local wezterm = require("wezterm")
 
 return {
 	enable_wayland = false,
-	font = wezterm.font("Iosevka Nerd Font Mono"),
+	-- family = "Iosevka Nerd Font Mono",
+	font = wezterm.font("FantasqueSansM Nerd Font Mono"),
 	font_size = 13,
 	enable_tab_bar = false,
 	window_background_opacity = 0.95,
+	colors = {
+		background = "#151515",
+	},
 	-- color_scheme = "Black Metal (Bathory) (base16)",
-	color_scheme = "Rosé Pine (base16)",
+	-- color_scheme = "Rosé Pine (base16)",
+  color_scheme = "rose-pine",
 	adjust_window_size_when_changing_font_size = false,
 	default_cursor_style = "BlinkingBar",
 	cursor_blink_rate = 500,
